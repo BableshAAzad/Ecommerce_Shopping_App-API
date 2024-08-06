6fc41bae7f1bb88072c952b98c11d13b2d064f63
bableshaazad <bableshaazad@outlook.com>
2024-08-06 14:27:53 +0530

    image update is done

--- src/main/java/com/ecommerce/shopping/image/repository/ImageRepository.java
+++ src/main/java/com/ecommerce/shopping/image/repository/ImageRepository.java
@@ -1,7 +1,12 @@
 package com.ecommerce.shopping.image.repository;
 
 import com.ecommerce.shopping.image.entity.Image;
+import com.ecommerce.shopping.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    List<Image> findByProduct(Product product);
 }

e96b9ce754ad67aaf9c1a5040de61288b4fc1047
bableshaazad <bableshaazad@outlook.com>
2024-08-17 21:32:28 +0530

    chane base url

---
+++ src/main/java/com/ecommerce/shopping/enums/BaseUrl.java
@@ -0,0 +1,6 @@
+package com.ecommerce.shopping.enums;
+
+public class BaseUrl {
+    public static final String LOCAL_BASE_URL = "http://localhost:8081/api/v1/";
+    public static final String BASE_URL = "https://storehousemanagementsystem-production.up.railway.app/api/v1/";
+}

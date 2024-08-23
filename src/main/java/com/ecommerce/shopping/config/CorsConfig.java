d96badbd0dd0ef697407e56105c7ba801555c440
bableshaazad <bableshaazad@outllok.com>
2024-08-23 16:26:50 +0530

    change cross config

--- src/main/java/com/ecommerce/shopping/config/CorsConfig.java
+++ src/main/java/com/ecommerce/shopping/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:5173",
-                        "https://bableshaazad-e-commerce-shopping-app.vercel.app")
+                        "https://ecommerce.bableshaazad.com")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");

3728e40822a1a21bd4431d88c6f1fd783420ac07
bableshaazad <bableshaazad@outllok.com>
2024-08-24 15:29:27 +0530

    update cors 1.3

--- src/main/java/com/ecommerce/shopping/config/CorsConfig.java
+++ src/main/java/com/ecommerce/shopping/config/CorsConfig.java
@@ -1,22 +1,42 @@
 package com.ecommerce.shopping.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig{
+//public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
 //                .allowedOriginPatterns("http://localhost:5173",
 //                        "https://ecommerce.bableshaazad.com",
 //                        "https://www.ecommerce.bableshaazad.com")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true)
-                .allowedHeaders("*")
-                .maxAge(3600);
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowCredentials(true)
+//                .allowedHeaders("*");
+//    }
+
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173",
+                        "https://ecommerce.bableshaazad.com",
+                        "https://www.ecommerce.bableshaazad.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
+
 }
